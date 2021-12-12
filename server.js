278a518709960bb627e54f234222d00d29baa93e
Alyssa May <may@alyss.am>
2021-12-11 21:41:33 -0500

    fix: chat.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //var cors = require('cors')
-import ***REMOVED*** ApiClient ***REMOVED*** from '@twurple/api';
-import ***REMOVED*** RefreshingAuthProvider , StaticAuthProvider, ClientCredentialsAuthProvider ***REMOVED*** from '@twurple/auth';
-import ***REMOVED*** NgrokAdapter ***REMOVED*** from '@twurple/eventsub-ngrok';
-import ***REMOVED*** ChatClient ***REMOVED*** from '@twurple/chat';
-import ***REMOVED*** DirectConnectionAdapter, EventSubListener ***REMOVED*** from '@twurple/eventsub';
-import ***REMOVED***promises as fs***REMOVED*** from 'fs';
+import { ApiClient } from '@twurple/api';
+import { RefreshingAuthProvider , StaticAuthProvider, ClientCredentialsAuthProvider } from '@twurple/auth';
+import { NgrokAdapter } from '@twurple/eventsub-ngrok';
+import { ChatClient } from '@twurple/chat';
+import { DirectConnectionAdapter, EventSubListener } from '@twurple/eventsub';
+import {promises as fs} from 'fs';
 import express from 'express';
 import express_ws from 'express-ws';
 var app = express();
@@ -19,23 +19,23 @@ const accessToken = config.accessToken;
 const clientSecret = config.clientSecret;
 console.log("Getting chat auth provider");
 const chatAuthProvider = new RefreshingAuthProvider(
-    ***REMOVED***
+    {
 		clientId,
 		clientSecret,
-		onRefresh: async newTokenData => ***REMOVED***
+		onRefresh: async newTokenData => {
             config.token = newTokenData;
             await fs.writeFile('./config.json', JSON.stringify(config, null, 4), 'UTF-8');
-        ***REMOVED***
-	***REMOVED***,
+        }
+	},
 	config.token
 );
 console.log("Getting app token auth provider");
 const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
-const apiClient = new ApiClient(***REMOVED*** authProvider ***REMOVED***);
+const apiClient = new ApiClient({ authProvider });
 console.log("Getting user id");
 const userId = (await apiClient.users.getUserByName('allymonies')).id;
 
-const chatClient = new ChatClient(***REMOVED*** authProvider: chatAuthProvider,  channels: ['allymonies'] ***REMOVED***);
+const chatClient = new ChatClient({ authProvider: chatAuthProvider,  channels: ['allymonies'] });
 // listen to more events...
 console.log("Connecting to chat");
 await chatClient.connect();
@@ -45,75 +45,75 @@ await chatClient.connect();
 const port = 3000;//config.port;
 const host = "127.0.0.1"
 
-/*var corsOptions = ***REMOVED***
+/*var corsOptions = {
   origin: config.website,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-***REMOVED****/
+}*/
 
 //app.use(cors(corsOptions))
-app.use(express.static('static',***REMOVED***index:"index.html",extensions:['html']***REMOVED***));
+app.use(express.static('static',{index:"index.html",extensions:['html']}));
 app.use(express.static('static'));
 
 const clients = [];
 
-app.ws('/ws', function(ws, req) ***REMOVED***
-    ws.on('message', function(msg) ***REMOVED***
+app.ws('/ws', function(ws, req) {
+    ws.on('message', function(msg) {
       console.log(msg);
-    ***REMOVED***);
+    });
     console.log('Websocket connection');
-***REMOVED***);
+});
 const wsServer = expressWs.getWss('/ws');
 
-function broadcast(payload) ***REMOVED***
-    wsServer.clients.forEach(function (client) ***REMOVED***
+function broadcast(payload) {
+    wsServer.clients.forEach(function (client) {
         client.send(JSON.stringify(payload));
-    ***REMOVED***);
-***REMOVED***
+    });
+}
 
-const chatListener = chatClient.onMessage(async (channel, user, message, msg) => ***REMOVED***
+const chatListener = chatClient.onMessage(async (channel, user, message, msg) => {
     console.log("Got message", message, "from", user);
-    if (message[0] == '!') ***REMOVED***
+    if (message[0] == '!') {
         const command = message.substr(1).split(' ');
         console.log("Got command", command);
-        if (command[0] == 'jump') ***REMOVED***
-            broadcast(***REMOVED***"type": "command", "command": "jump"***REMOVED***);
-        ***REMOVED*** else if (command[0] == 'spin' && admins.includes(user.toLowerCase())) ***REMOVED***
-            broadcast(***REMOVED***"type": "command", "command": "spin"***REMOVED***);
-        ***REMOVED*** else if (command[0] == 'testfollow' && command.length > 1 && admins.includes(user.toLowerCase())) ***REMOVED***
-            broadcast(***REMOVED***"type": "follow", "user": command[1], "displayName": command[1]***REMOVED***);
-        ***REMOVED*** else if (command[0] == 'flip' && admins.includes(user.toLowerCase())) ***REMOVED***
-            broadcast(***REMOVED***"type": "command", "command": "flip"***REMOVED***);
-        ***REMOVED***
-    ***REMOVED*** else ***REMOVED***
-        broadcast(***REMOVED***"type": "message", "user": user, "message": message***REMOVED***);
-    ***REMOVED***
-***REMOVED***);
+        if (command[0] == 'jump') {
+            broadcast({"type": "command", "command": "jump"});
+        } else if (command[0] == 'spin' && admins.includes(user.toLowerCase())) {
+            broadcast({"type": "command", "command": "spin"});
+        } else if (command[0] == 'testfollow' && command.length > 1 && admins.includes(user.toLowerCase())) {
+            broadcast({"type": "follow", "user": command[1], "displayName": command[1]});
+        } else if (command[0] == 'flip' && admins.includes(user.toLowerCase())) {
+            broadcast({"type": "command", "command": "flip"});
+        }
+    } else {
+        broadcast({"type": "message", "user": user, "message": message});
+    }
+});
 
 apiClient.eventSub.deleteAllSubscriptions()
-const listener = new EventSubListener(***REMOVED***
+const listener = new EventSubListener({
     apiClient,
     adapter: new NgrokAdapter(),
     secret: config.eventSubSecret
-***REMOVED***);
+});
 console.log("Starting event listener");
 await listener.listen();
 
 console.log("Creating subscriptions");
-const followSubscription = await listener.subscribeToChannelFollowEvents(userId, e => ***REMOVED***
+const followSubscription = await listener.subscribeToChannelFollowEvents(userId, e => {
     const name = e.userDisplayName ?? e.userName;
     console.log(name, " followed!");
-    broadcast(***REMOVED***"type": "follow", "user": e.userName, "displayName": name***REMOVED***);
-***REMOVED***);
+    broadcast({"type": "follow", "user": e.userName, "displayName": name});
+});
 
-const channelUpdateSupscription = await listener.subscribeToChannelUpdateEvents(userId, e => ***REMOVED***
+const channelUpdateSupscription = await listener.subscribeToChannelUpdateEvents(userId, e => {
     console.log("Got a channel update event");
-    /*wsServer.clients.forEach(function (client) ***REMOVED***
-        client.send(JSON.stringify(***REMOVED***"type": "follow"***REMOVED***));
-    ***REMOVED***);*/
-***REMOVED***);
+    /*wsServer.clients.forEach(function (client) {
+        client.send(JSON.stringify({"type": "follow"}));
+    });*/
+});
 
-var server = app.listen(port, host, function () ***REMOVED***
+var server = app.listen(port, host, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("CHAT CUBE CHAT CUBE on", host, port);
-***REMOVED***)
+})
--- static/chat.js
+++ static/chat.js
@@ -23,7 +23,7 @@ let cubeNod = 0;
 let cubeRot = 0;
 let bpm = 0;
 
-function escapeHTML (unsafe_str) ***REMOVED***
+function escapeHTML (unsafe_str) {
     return unsafe_str
       .replace(/&/g, '&amp;')
       .replace(/</g, '&lt;')
@@ -31,134 +31,134 @@ function escapeHTML (unsafe_str) ***REMOVED***
       .replace(/\"/g, '&quot;')
       .replace(/\'/g, '&#39;')
       .replace(/\//g, '&#x2F;')
-***REMOVED***
+}
 
-function changeSide() ***REMOVED***
+function changeSide() {
     var showClass = 'show-front';
-    if ( currentClass ) ***REMOVED***
+    if ( currentClass ) {
         cube.classList.remove( currentClass );
-    ***REMOVED***
+    }
     cube.classList.add( showClass );
     currentClass = showClass;
-***REMOVED***
+}
 
-function setAlert(text) ***REMOVED***
+function setAlert(text) {
     const alertBoxes = document.getElementsByClassName("alertbox");
     const alerts = document.getElementsByClassName("alert");
-    for (var i = 0; i < alertBoxes.length; i++) ***REMOVED***
+    for (var i = 0; i < alertBoxes.length; i++) {
         alerts[i].innerHTML = escapeHTML(text);
-        if (text == "") ***REMOVED***
+        if (text == "") {
             alertBoxes[i].classList.add("hidden");
-        ***REMOVED*** else ***REMOVED***
+        } else {
             alertBoxes[i].classList.remove("hidden");
-        ***REMOVED***
-    ***REMOVED***
-    if (text != "") ***REMOVED***
+        }
+    }
+    if (text != "") {
         alertTime = alertDuration;
         alertActive = true;
-    ***REMOVED***
-***REMOVED***
+    }
+}
 
 setAlert("");
 // set initial side
 changeSide();
 
-(function() ***REMOVED***
+(function() {
     var chats = document.getElementsByClassName('chat');
     var emma = document.getElementById('emma');
     var videoWidth = 200;
     var videoHeight = 200;
 
-    function getCamera(width, height, camera, callback) ***REMOVED***
+    function getCamera(width, height, camera, callback) {
         //var compatibleBrowser = hasUserMedia();
-        navigator.mediaDevices.enumerateDevices().then(function(devices) ***REMOVED***
+        navigator.mediaDevices.enumerateDevices().then(function(devices) {
             var device = null;
-            for (let i = 0; i !== devices.length; ++i) ***REMOVED***
+            for (let i = 0; i !== devices.length; ++i) {
                 const deviceCheck = devices[i];
-                if (deviceCheck.kind === 'videoinput' && deviceCheck.label === camera) ***REMOVED***
+                if (deviceCheck.kind === 'videoinput' && deviceCheck.label === camera) {
                     device = deviceCheck.deviceId;
-                ***REMOVED***
-            ***REMOVED***
+                }
+            }
             console.log(devices);
             var videoResolution = getBrowserVideoSettings("", width, height, device);
             console.log(videoResolution);
 
             window.URL = window.URL || window.webkitURL;
 
-            navigator.getUserMedia(***REMOVED***
+            navigator.getUserMedia({
                 audio: false,
                 video: videoResolution
-        ***REMOVED***
-            function(stream) ***REMOVED***
+            },
+            function(stream) {
                 callback(stream);
                 //video.srcObject = stream;
                 //video.src = window.URL.createObjectURL(stream);
-        ***REMOVED***
-            function(err) ***REMOVED***
+            },
+            function(err) {
                 console.log("The following error occured: " + err.name);
-            ***REMOVED***);
-        ***REMOVED***);      
-    ***REMOVED***
+            });
+        });      
+    }
 
-  function getBrowserVideoSettings(browser, width, height, device) ***REMOVED***
-      if (browser === navigator.mozGetUserMedia) ***REMOVED***
-          return ***REMOVED***
+  function getBrowserVideoSettings(browser, width, height, device) {
+      if (browser === navigator.mozGetUserMedia) {
+          return {
               /*width: width,
               height: height,*/
-              deviceId: ***REMOVED*** exact: device ***REMOVED***
-          ***REMOVED***
-      ***REMOVED*** else ***REMOVED***
-          return ***REMOVED***
+              deviceId: { exact: device }
+          }
+      } else {
+          return {
               /*minWidth: width,
               minHeight: height,*/
-              deviceId: device ? ***REMOVED*** exact: device ***REMOVED*** : undefined
-          ***REMOVED***
-      ***REMOVED***
-    ***REMOVED***
-    getCamera(videoWidth, videoHeight, "OBS-Camera4", function (stream) ***REMOVED***
+              deviceId: device ? { exact: device } : undefined
+          }
+      }
+    }
+    getCamera(videoWidth, videoHeight, "OBS-Camera4", function (stream) {
       emma.srcObject = stream;
-    ***REMOVED***);
+    });
     
-    getCamera(videoWidth, videoHeight, "OBS-Camera3", function (stream) ***REMOVED***
-      for (let i = 0; i < chats.length; i++) ***REMOVED***
+    getCamera(videoWidth, videoHeight, "OBS-Camera3", function (stream) {
+      for (let i = 0; i < chats.length; i++) {
           chats[i].srcObject = stream;
-      ***REMOVED***
-    ***REMOVED***);
+      }
+    });
 
-***REMOVED***)();
+})();
 
-function getAudioSource(deviceLabel, callback) ***REMOVED***
+function getAudioSource(deviceLabel, callback) {
   //var compatibleBrowser = hasUserMedia();
-  navigator.mediaDevices.enumerateDevices().then(function(devices) ***REMOVED***
+  navigator.mediaDevices.enumerateDevices().then(function(devices) {
       var device = null;
-      for (let i = 0; i !== devices.length; ++i) ***REMOVED***
+      for (let i = 0; i !== devices.length; ++i) {
           const deviceCheck = devices[i];
-          if (deviceCheck.kind === 'audioinput' && deviceCheck.label === deviceLabel) ***REMOVED***
+          if (deviceCheck.kind === 'audioinput' && deviceCheck.label === deviceLabel) {
               device = deviceCheck.deviceId;
-          ***REMOVED***
-      ***REMOVED***
+          }
+      }
 
       window.URL = window.URL || window.webkitURL;
       console.log("Getting device", device)
-      navigator.getUserMedia(***REMOVED***
-          audio: ***REMOVED***
+      navigator.getUserMedia({
+          audio: {
                 deviceId: device
-          ***REMOVED***
-  ***REMOVED***
-      function(stream) ***REMOVED***
+          }
+      },
+      function(stream) {
           callback(stream);
           //video.srcObject = stream;
           //video.src = window.URL.createObjectURL(stream);
-  ***REMOVED***
-      function(err) ***REMOVED***
+      },
+      function(err) {
           console.log("The following error occured: " + err.name);
-      ***REMOVED***);
-  ***REMOVED***);      
-***REMOVED***
+      });
+  });      
+}
 
 /* THANK YOU to JMPerez for this BPM detector you are amazin <3 */
 
-function getPeaks(data) ***REMOVED***
+function getPeaks(data) {
 
     // What we're going to do here, is to divide up our audio into parts.
 
@@ -180,25 +180,25 @@ function getPeaks(data) ***REMOVED***
         parts = data[0].length / partSize,
         peaks = [];
 
-    for (var i = 0; i < parts; i++) ***REMOVED***
+    for (var i = 0; i < parts; i++) {
         var max = 0;
-        for (var j = i * partSize; j < (i + 1) * partSize; j++) ***REMOVED***
+        for (var j = i * partSize; j < (i + 1) * partSize; j++) {
             var volume = Math.max(Math.abs(data[0][j]), Math.abs(data[1][j]));
-            if (!max || (volume > max.volume)) ***REMOVED***
-                max = ***REMOVED***
+            if (!max || (volume > max.volume)) {
+                max = {
                     position: j,
                     volume: volume
-                ***REMOVED***;
-            ***REMOVED***
-        ***REMOVED***
+                };
+            }
+        }
         peaks.push(max);
-    ***REMOVED***
+    }
 
     // We then sort the peaks according to volume...
 
-    peaks.sort(function(a, b) ***REMOVED***
+    peaks.sort(function(a, b) {
         return b.volume - a.volume;
-    ***REMOVED***);
+    });
 
     // ...take the loundest half of those...
 
@@ -206,14 +206,14 @@ function getPeaks(data) ***REMOVED***
 
     // ...and re-sort it back based on position.
 
-    peaks.sort(function(a, b) ***REMOVED***
+    peaks.sort(function(a, b) {
         return a.position - b.position;
-    ***REMOVED***);
+    });
 
     return peaks;
-***REMOVED***
+}
 
-function getIntervals(peaks) ***REMOVED***
+function getIntervals(peaks) {
 
     // What we now do is get all of our peaks, and then measure the distance to
     // other peaks, to create intervals.  Then based on the distance between
@@ -225,35 +225,35 @@ function getIntervals(peaks) ***REMOVED***
 
     var groups = [];
 
-    peaks.forEach(function(peak, index) ***REMOVED***
-        for (var i = 1; (index + i) < peaks.length && i < 10; i++) ***REMOVED***
-            var group = ***REMOVED***
+    peaks.forEach(function(peak, index) {
+        for (var i = 1; (index + i) < peaks.length && i < 10; i++) {
+            var group = {
                 tempo: (60 * 44100) / (peaks[index + i].position - peak.position),
                 count: 1
-            ***REMOVED***;
+            };
 
-            while (group.tempo < 90) ***REMOVED***
+            while (group.tempo < 90) {
                group.tempo *= 2;
-            ***REMOVED***
+            }
 
-            while (group.tempo > 180) ***REMOVED***
+            while (group.tempo > 180) {
                group.tempo /= 2;
-            ***REMOVED***
+            }
 
             group.tempo = Math.round(group.tempo);
 
-            if (!(groups.some(function(interval) ***REMOVED***
+            if (!(groups.some(function(interval) {
                 return (interval.tempo === group.tempo ? interval.count++ : 0);
-            ***REMOVED***))) ***REMOVED***
+            }))) {
                 groups.push(group);
-            ***REMOVED***
-        ***REMOVED***
-    ***REMOVED***);
+            }
+        }
+    });
     return groups;
-***REMOVED***
+}
 
 
-var initializeRecorder = function(stream) ***REMOVED***
+var initializeRecorder = function(stream) {
     var audioContext = window.AudioContext;
     var context = new audioContext();
     var bufferSize = 512;
@@ -278,73 +278,73 @@ var initializeRecorder = function(stream) ***REMOVED***
 
     audioInput.connect(recorder);
     recorder.connect(context.destination);
-***REMOVED***
+}
 
-var recorderProcess = function(e) ***REMOVED***
+var recorderProcess = function(e) {
     var floatData = e.inputBuffer.getChannelData(0);
     rollingBufferA.push(...floatData);
-    if (rollingBufferA.length > bufferSize) ***REMOVED***
+    if (rollingBufferA.length > bufferSize) {
        rollingBufferA.splice(0, rollingBufferA.length - bufferSize);
-    ***REMOVED***
+    }
 
     var peaks = getPeaks([rollingBufferA, rollingBufferA]);
     var groups = getIntervals(peaks);
-    var top = groups.sort(function(intA, intB) ***REMOVED***
+    var top = groups.sort(function(intA, intB) {
         return intB.count - intA.count;
-    ***REMOVED***).splice(0, 5);
+    }).splice(0, 5);
 
     bpm = top[0].tempo;
     let max = 0;
-    for (var i = 0; i < floatData.length; i++) ***REMOVED***
-      if (floatData[i] > max) ***REMOVED***
+    for (var i = 0; i < floatData.length; i++) {
+      if (floatData[i] > max) {
         max = floatData[i];
-      ***REMOVED***
-    ***REMOVED***
+      }
+    }
     averageWindow.push(max);
-    if (averageWindow.length > windowSize) ***REMOVED***
+    if (averageWindow.length > windowSize) {
       averageWindow.shift();
-    ***REMOVED***
+    }
     let average = 0;
-    for (var i = 0; i < averageWindow.length; i++) ***REMOVED***
+    for (var i = 0; i < averageWindow.length; i++) {
       average += averageWindow[i];
-    ***REMOVED***
+    }
     average /= averageWindow.length;
     vol = average * 30;
-***REMOVED***
+}
 
-var convertAudio = function(buffer) ***REMOVED***
+var convertAudio = function(buffer) {
     l = buffer.length;
     buf = new Int16Array(l);
-    while (l--) ***REMOVED***
+    while (l--) {
         buf[l] = Math.min(1, buffer[l])*0x7FFF;
-    ***REMOVED***
+    }
     return buf;
-***REMOVED***
+}
 
-function easeInOutCubic(x) ***REMOVED***
+function easeInOutCubic(x) {
     return x < 0.5 ? 4 * x * x * x : 1 - Math.pow(-2 * x + 2, 3) / 2;
-***REMOVED***
+}
 
 getAudioSource("Microphone (VB-Audio Virtual Cable)", initializeRecorder);
 
 let jump = 0;
 let spin = 0;
-setInterval(function() ***REMOVED***
+setInterval(function() {
     cubeRot += (spinDirection ? 1 : -1) * (updateRate / msBetweenRotations) * (2 * Math.PI);
     //cubeRot += 0.002;
-    if (spinDirection && cubeRot > (2 * Math.PI)) ***REMOVED***
+    if (spinDirection && cubeRot > (2 * Math.PI)) {
         cubeRot -= (2 * Math.PI);
-    ***REMOVED*** else if (!spinDirection && cubeRot < 0) ***REMOVED***
+    } else if (!spinDirection && cubeRot < 0) {
         cubeRot += (2 * Math.PI);
-    ***REMOVED***
+    }
     const msBetweenBeats = (60000 / bpm);
-    if (isFinite(msBetweenBeats)) ***REMOVED***
+    if (isFinite(msBetweenBeats)) {
         const add = (updateRate / msBetweenBeats) * Math.PI; 
         cubeNod += add;
-        if (cubeNod > (2 * Math.PI)) ***REMOVED***
+        if (cubeNod > (2 * Math.PI)) {
             cubeNod -= (2 * Math.PI);
-        ***REMOVED***
-    ***REMOVED***
+        }
+    }
     let maxNod = (easeInOutCubic(Math.min(vol+.20,1)) * (1 + Math.log(Math.max(1, vol)))) * 0.2;
 
     const nod = Math.sin(cubeNod)*maxNod;
@@ -358,40 +358,40 @@ setInterval(function() ***REMOVED***
     const rot = cubeRot + spin;
     document.getElementById("debug").innerHTML = bpmString + ", " + nodString + "%, " + volString + "%, " + jumpString.toString();
     cube.style.transform = "translateY(" + ((Math.cos(Math.PI + jump)+1)*-(jumpHeight / 2)).toString() + "px) translateZ(-100px) rotateY(" + rot.toString() + "rad) rotateX(" + nodX.toString() + "rad) rotateZ(" + nodZ.toString() + "rad)";
-***REMOVED***, updateRate)
+}, updateRate)
 
-setInterval(function() ***REMOVED***
-    if (alertActive) ***REMOVED***
+setInterval(function() {
+    if (alertActive) {
         alertTime -= updateRate;
-        if (alertTime <= 0) ***REMOVED***
+        if (alertTime <= 0) {
             alertActive = false;
             alertTime = 0;
             setAlert("");
-        ***REMOVED***
-    ***REMOVED***
-    if (jump > 0) ***REMOVED***
+        }
+    }
+    if (jump > 0) {
         jump -= (updateRate / jumpDuration) * (2 * Math.PI);
-    ***REMOVED***
-    if (spin > 0) ***REMOVED***
+    }
+    if (spin > 0) {
         spin -= (updateRate / spinDuration) * ((numberOfSpins * 2) * Math.PI);
-    ***REMOVED***
-***REMOVED***, updateRate)
+    }
+}, updateRate)
 
-webSocket.onmessage = function (event) ***REMOVED***
+webSocket.onmessage = function (event) {
     const msg = JSON.parse(event.data);
-    if (msg.type == "message") ***REMOVED***
+    if (msg.type == "message") {
         jump = Math.PI*2;
-    ***REMOVED*** else if (msg.type == "follow") ***REMOVED***
+    } else if (msg.type == "follow") {
         spin = Math.PI * (numberOfSpins * 2);
         setAlert(msg.displayName + " has followed!");
         notificationSound.play();
-    ***REMOVED*** else if (msg.type == "command") ***REMOVED***
-        if (msg.command == "spin") ***REMOVED***
+    } else if (msg.type == "command") {
+        if (msg.command == "spin") {
             spin = Math.PI * (numberOfSpins * 2);
-        ***REMOVED*** else if (msg.command == "jump") ***REMOVED***
+        } else if (msg.command == "jump") {
             jump = Math.PI*2;
-        ***REMOVED*** else if (msg.command == "flip") ***REMOVED***
+        } else if (msg.command == "flip") {
             spinDirection = !spinDirection;
-        ***REMOVED***
-    ***REMOVED***
-***REMOVED***
+        }
+    }
+}
